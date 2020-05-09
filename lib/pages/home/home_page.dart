481d57cb111c97ececabfebfe148d266c36ed009
Lyon Liang <lyon.liang12@gmail.com>
2020-05-09 17:44:27 +0800

    add display block

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -3,9 +3,43 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-          appBar: AppBar(title: Text('首页')), body: Center(child: Text('首页'))),
+    return Scaffold(
+      appBar: AppBar(title: Text('首页')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            height: 100,
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              gradient: LinearGradient(
+                colors: [
+                  Colors.green,
+                  Colors.greenAccent,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(4, 4),
+                ),
+              ],
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            alignment: Alignment.center,
+            child: Text(
+              '\$1200',
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
