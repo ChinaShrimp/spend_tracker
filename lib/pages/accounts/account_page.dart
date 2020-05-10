2cce960c63eac798ee4c73b0efb51c83b06eca75
Lyon Liang <lyon.liang12@gmail.com>
2020-05-10 08:07:13 +0800

    add account page

--- lib/pages/accounts/account_page.dart
+++ lib/pages/accounts/account_page.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatefulWidget {
+  @override
+  _AccountPageState createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  Map<String, dynamic> _data = Map<String, dynamic>();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Account')),
+        appBar: AppBar(
+          title: Text('Account'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                if (!_formKey.currentState.validate()) return;
+
+                _formKey.currentState.save();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
         body: Center(
-          child: Text('Account'),
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        width: 2,
+                        color: Colors.blueGrey,
+                      ))),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '姓名'),
+                    validator: (String value) {
+                      if (value.isEmpty) return '必填项';
+                    },
+                    onSaved: (String value) => _data['name'] = value,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.numberWithOptions(
+                      decimal: true,
+                    ),
+                    decoration: InputDecoration(labelText: '余额'),
+                    validator: (String value) {
+                      if (value.isEmpty) return '必填项';
+
+                      if (double.tryParse(value) == null) return '不合法的数字';
+                    },
+                    onSaved: (String value) => _data['balance'] = value,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ));
   }
 }
--- lib/pages/accounts/accounts_page.dart
+++ lib/pages/accounts/accounts_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'account_page.dart';
+
 class AccountsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Accounts')),
+        appBar: AppBar(
+          title: Text('Accounts'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AccountPage(),
+                    ));
+              },
+            ),
+          ],
+        ),
         body: Center(
           child: Text('Accounts'),
         ));
